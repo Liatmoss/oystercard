8cf03e305b7ccadcfeee5de84616fd45836b3450
liatmoss <liatmoss@hotmail.com>
2019-10-21 15:18:02 +0100

    Added test and code for mex limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,14 @@ class Oystercard
 
 attr_reader :balance
 
+MAX_BALANCE = 30
+
 def initialize
   @balance = 0
 end
 
 def top_up(amount)
+  fail "Over the limit of #{MAX_BALANCE}" if balance + amount > MAX_BALANCE
   @balance += amount
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,10 @@ describe Oystercard do
     expect(subject.balance).to eq 2
   end
 
+  it 'should raise an error if balance exceeds maximum balance'do
+    maximum_balance = Oystercard::MAX_BALANCE
+    subject.top_up(maximum_balance)
+    expect { subject.top_up 1 }.to raise_error "Over the limit of #{maximum_balance}"
+  end
+
 end
