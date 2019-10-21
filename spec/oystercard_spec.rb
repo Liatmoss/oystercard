248a86369dee55b079da54bd399fb308b607dc00
Emma Van Oss <e.vanoss@gmail.com>
2019-10-21 15:35:49 +0100

    improved deduction test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,15 +11,15 @@ describe Oystercard do
     expect(subject.balance).to eq 2
   end
 
-  it 'should raise an error if balance exceeds maximum balance'do
+  it 'should raise an error if balance exceeds maximum balance' do
     maximum_balance = Oystercard::MAX_BALANCE
     subject.top_up(maximum_balance)
     expect { subject.top_up 1 }.to raise_error "Over the limit of #{maximum_balance}"
   end
 
-  it 'reduces balance when deduct is called' do
-    subject.top_up(2)
-    expect(subject.deduct(1)).to eq 1
+  it 'reduces balance by amount when deduct is called' do
+    subject.top_up(5)
+    expect { subject.deduct(3) }.to change { subject.balance }.by -3
   end
 
 end
