16a9f99f61710a6ff6749a286bff3918dce17295
Emma Van Oss <e.vanoss@gmail.com>
2019-10-21 14:41:23 +0100

    top_up(amount) increases balance by amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it 'balance increases by 2 when top_up(2) is called' do
+    subject.top_up(2)
+    expect(subject.balance).to eq 2
+  end
+
 end
